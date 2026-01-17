daa57f656698f18566764ebb033fcb1a216cd06a
angelop47 <pescettoangelo@gmail.com>
2026-01-17 12:15:37 -0300

    feat: integrate astro-icon in config and Features component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://astro-starter.devgelo.com/",
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
   prefetch: {
     prefetchAll: true,
     defaultStrategy: "viewport",
