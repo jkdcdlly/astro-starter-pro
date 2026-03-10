d30ee09e4cd5d754eed6745bf98cd4b5bc0bc809
qiyuyunlei <15199427+qiyuyunlei@user.noreply.gitee.com>
2026-03-10 09:25:23 +0800

    修改多语言

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,11 +1,9 @@
 import ogImage from "../assets/og-image.png";
+import { Locale, ui } from "../i18n";
 
 export const siteConfig = {
   name: "War 3 Maps",
-  description: "不只是地图下载站，而是属于所有War3玩家和作者的共同家园",
   url: "https://war3maps.net",
-  lang: "en",
-  locale: "en_US",
   author: "War3Maps",
   twitter: "@War3Maps",
   ogImage: ogImage,
@@ -14,12 +12,20 @@ export const siteConfig = {
     github: "https://github.com/devgelo-labs/astro-starter-pro",
     discord: "https://discord.gg/Uk6xsSFE",
   },
-  navLinks: [
-    { text: "Home", href: "/" },
-    { text: "About", href: "/about" },
-    { text: "Services", href: "/services" },
-    { text: "Blog", href: "/blog" },
-    { text: "Contact", href: "/contact" },
-    { text: "Widgets", href: "/widgets" },
-  ],
+  getDescription: (lang: Locale = "en") => {
+    return ui[lang].site?.description || "不只是地图下载站，而是属于所有War3玩家和作者的共同家园";
+  },
+  getNavLinks: (lang: Locale = "en") => {
+    return ui[lang].site?.nav || [
+      { text: "Home", href: "/" },
+      { text: "About", href: "/about" },
+      { text: "Services", href: "/services" },
+      { text: "Blog", href: "/blog" },
+      { text: "Contact", href: "/contact" },
+      { text: "Widgets", href: "/widgets" },
+    ];
+  },
+  getLocale: (lang: Locale = "en") => {
+    return lang === "zh" ? "zh_CN" : "en_US";
+  },
 };
